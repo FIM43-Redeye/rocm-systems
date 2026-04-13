f59e56e4ecdd27ff662c04d738b1df3e0de2b6da
Abhinab Paul Choudhury <abhinab.choudhury@amd.com>
2026-04-13 02:03:43 -0500

    Separated metric expression builder

--- projects/rocprofiler-compute/src/utils/metrics/expression.py
+++ projects/rocprofiler-compute/src/utils/metrics/expression.py
@@ -109,6 +109,34 @@ def build_eval_string(equation: str, coll_level: str, config: dict) -> str:
             ) / to_sum(
                 raw_pmc_df["pmc_perf"]["TCC_EA_RDREQ_31"]
             )
+        input:
+            AVG(100  * SQ_ACTIVE_INST_SCA / ( GRBM_GUI_ACTIVE * $numCU ))
+        output:
+            to_avg(
+                100 * raw_pmc_df["pmc_perf"]["SQ_ACTIVE_INST_SCA"] /
+                (
+                    raw_pmc_df["pmc_perf"]["GRBM_GUI_ACTIVE"] *
+                    numCU
+                )
+            )
+        input:
+            AVG(
+                (
+                    TCC_EA_RDREQ_LEVEL_31 / TCC_EA_RDREQ_31
+                )
+                if (TCC_EA_RDREQ_31 != 0)
+                else (0)
+            )
+        output:
+            to_avg(
+                (
+                    raw_pmc_df["pmc_perf"]["TCC_EA_RDREQ_LEVEL_31"] /
+                    raw_pmc_df["pmc_perf"]["TCC_EA_RDREQ_31"]
+                ).where(
+                    raw_pmc_df["pmc_perf"]["TCC_EA_RDREQ_31"] != 0,
+                    0
+                )
+            )
         We can not handle the below for now:
         input:
             AVG(
